4473643bbe4dda5b37cc7c518b3a38c9d05a2741
KarinaShtankova <shtankovads@gmail.com>
2023-12-11 14:38:10 +0200

    bugfix  task 02
    adds alert and trim()

--- src/js/02-form.js
+++ src/js/02-form.js
@@ -24,13 +24,14 @@ function onformRefInput(e) {
 const savedInputData = localStorage.getItem(localStorageKey);
 const parsedInputData = JSON.parse(savedInputData);
 
-// console.log(parsedInputData);
+console.log(parsedInputData);
 
 const textarea = formRef.elements.message;
 const email = formRef.elements.email;
 
 window.addEventListener('load', e => {
   if (parsedInputData) {
+
     textarea.value = parsedInputData.message;
     email.value = parsedInputData.email;
     inputData.email = parsedInputData.email;
@@ -38,13 +39,18 @@ window.addEventListener('load', e => {
   }
 });
 
-formRef.addEventListener('submit', onformRefSubmit);
 
-function onformRefSubmit(e) {
-  e.preventDefault();
-  console.log(inputData);
-  localStorage.removeItem(localStorageKey);
-  inputData.email = '';
-  inputData.message = '';
-  formRef.reset();
-}
+  formRef.addEventListener('submit', onformRefSubmit);
+
+  function onformRefSubmit(e) {
+    e.preventDefault();
+   inputData.message = inputData.message.trim();
+    if (inputData.email.trim() === '' || inputData.message.trim() === '') return alert("Please fill in all the fields!");
+
+    console.log(inputData);
+    localStorage.removeItem(localStorageKey);
+    inputData.email = '';
+    inputData.message = '';
+    formRef.reset();
+  }
+
